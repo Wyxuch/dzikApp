dd2bdab46b70a701372879213adae04b24ab5c27
Wyxuch <50527152+Wyxuch@users.noreply.github.com>
2020-11-27 00:35:36 +0100

    initial commit

---
+++ src/views/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { style } from "";
+
+const TopNav = () => {
+    return (
+        <View style={style.TopNav}>
+
+        </View>
+    );
+};
+
+export default TopNav;
