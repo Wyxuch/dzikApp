e915cd06d4916957af636accae1f0acaf64a7332
Wyxuch <50527152+Wyxuch@users.noreply.github.com>
2020-11-27 19:18:36 +0100

    added nav elements

--- src/components/containers/TopNav.js
+++ src/components/containers/TopNav.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import style from "../../style";
 
 const TopNav = () => {
     return (
         <View style={{height: 60, ...style.Shadow}}>
             <View style={style.TopNav}>
-                <Text style={style.NavTitle}>DZIK!</Text>
+                <View></View>
+                <View style={style.LogoWrapper}>
+                    <Text style={style.NavTitle}>DZIK!</Text>
+                    <Image style={style.Logo} source={require('../../assets/images/logo.png')}/>
+                </View>
+                <TouchableOpacity onPress={()=>{alert("you clicked me")}}>
+                    <Image style={style.Logo} source={require('../../assets/images/menu.png')}/>
+                </TouchableOpacity>
             </View>
         </View>
     );
